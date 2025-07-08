f6e0df8c3ec6df87d54407b8089de950228e8dc3
SilenLoc <silen.locatelli@gmx.ch>
2025-07-08 23:19:46 +0200

    fix: remove target folder

--- scrapy/target/debug/deps/scrapy-77efe25c1a99700c.d
+++
@@ -1,5 +0,0 @@
-/home/silen/code/lunchtimecode/grocy/scrapy/target/debug/deps/scrapy-77efe25c1a99700c.d: src/lib.rs
-
-/home/silen/code/lunchtimecode/grocy/scrapy/target/debug/deps/libscrapy-77efe25c1a99700c.rmeta: src/lib.rs
-
-src/lib.rs:
--- scrapy/target/debug/deps/scrapy-a1a71b0d122a34fb.d
+++
@@ -1,5 +0,0 @@
-/home/silen/code/lunchtimecode/grocy/scrapy/target/debug/deps/scrapy-a1a71b0d122a34fb.d: src/lib.rs
-
-/home/silen/code/lunchtimecode/grocy/scrapy/target/debug/deps/libscrapy-a1a71b0d122a34fb.rmeta: src/lib.rs
-
-src/lib.rs:
